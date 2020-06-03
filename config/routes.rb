c7c26e26e7dbd0a91828aa29c41540bc57349036
mmadodo <mmadodo1@gmail.com>
2020-06-03 23:22:20 +0200

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'gigs/new'
+  get 'gigs/create'
+  get 'gigs/edit'
+  get 'gigs/update'
+  get 'gigs/show'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
